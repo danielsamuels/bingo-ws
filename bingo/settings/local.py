0df7148a2a0834b1394db33c6dd293194db8f8c6
Daniel Samuels <daniel@onespacemedia.com>
2016-08-19 17:02:45 +0100

    Remove a load of unused code.

--- bingo/settings/local.py
+++ bingo/settings/local.py
@@ -13,26 +13,17 @@ import os.path
 from .base import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
 # Run in debug mode.
-
 DEBUG = True
 
 # Save media files to the user's Sites folder.
-
 MEDIA_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "media"))
 STATIC_ROOT = os.path.expanduser(os.path.join("~/Sites", SITE_DOMAIN, "static"))
-NODE_MODULES_ROOT = os.path.expanduser(os.path.join("~/Workspace/bingo", "node_modules"))
 
 
 # Use local server.
-
-SITE_DOMAIN = "localhost:8000"
+SITE_DOMAIN = "127.0.0.1:8000"
 
 PREPEND_WWW = False
-
-
-# Disable the template cache for development.
-# TEMPLATES[0]['OPTIONS']['loaders'] = []
-
 
 # Optional separate database settings
 DATABASES = {
@@ -43,18 +34,3 @@ DATABASES = {
         "USER": os.getlogin(),
     },
 }
-
-# Mailtrip SMTP
-EMAIL_HOST = 'mailtrap.io'
-EMAIL_HOST_USER = '178288370161874a6'
-EMAIL_HOST_PASSWORD = '5033a6d5bca3f0'
-EMAIL_PORT = '2525'
-EMAIL_USE_TLS = True
-
-
-def frontend_templates():
-    return [
-        str(f[:-5])
-        for f in os.listdir(os.path.join(TEMPLATE_DIRS[0], 'frontend'))
-        if f[:1] != '_'
-    ]
--- bingo/urls.py
+++ bingo/urls.py
@@ -1,19 +1,10 @@
-"""URL config for bingo project."""
-
-# from cms.forms import CMSPasswordChangeForm
-# from cms.sitemaps import registered_sitemaps
-# from cms.views import TextTemplateView
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views import generic
 
-# from .apps.sections.models import sections_js
-from .utils.views import FrontendView
-
 admin.autodiscover()
-
 
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
@@ -27,9 +18,4 @@ if settings.DEBUG:
     urlpatterns += [
         url(r"^404/$", generic.TemplateView.as_view(template_name="404.html")),
         url(r"^500/$", generic.TemplateView.as_view(template_name="500.html")),
-        url(r'^frontend/$', FrontendView.as_view()),
-        url(r'^frontend/(?P<slug>[\w-]+)/$', FrontendView.as_view())
     ]
-
-
-handler500 = "cms.views.handler500"
